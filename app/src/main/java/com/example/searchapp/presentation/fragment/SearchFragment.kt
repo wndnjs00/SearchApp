02bc5d9becb2ccf49b3104898d01b1f28d08a006
wndnjs00 <juwonjeon852@gmail.com>
2024-05-07 21:15:53 +0900

    feat: sharedpreferences 적용중

--- app/src/main/java/com/example/searchapp/presentation/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/searchapp/presentation/fragment/SearchFragment.kt
@@ -1,11 +1,15 @@
 package com.example.searchapp.presentation.fragment
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
@@ -38,11 +42,13 @@ class SearchFragment : Fragment() {
         initSearchView()
         setRecyclerView()
 
+        // 데이터 업데이트
         searchViewModel.getSearchImageLiveData.observe(viewLifecycleOwner){
             Log.d("debugSearchData", it.toString())             // 전체 데이터
-            Log.d("test_thumbnailUrl", it[0].thumbnailUrl)      // 이미지 url
+
             this.searchAdapter.submitList(ArrayList(it))
         }
+
 
         return binding.root
     }
@@ -78,23 +84,41 @@ class SearchFragment : Fragment() {
                     searchViewModel.getSearchImageList(query)
                 }
 
+                sharedSave()
+
                 return false    // false : 검색 키보드를 내림, return true : 검색 키보드를 내리지 않음
             }
 
             // 검색창에서 글자 변경이 일어날 때마다 호출
             override fun onQueryTextChange(newText: String?): Boolean {
 
-//                if (newText != null) {
-//                    searchViewModel.getSearchImageList(newText)
-//                }
-
                 return true
             }
         }
-
         )
     }
 
 
+    private fun sharedSave() {
+        // fragment에서는 sharedPreferences 쓸려면 activtiy를 참조해서 써줘야함
+        val sharedPreferences = activity?.getSharedPreferences("searchText", Context.MODE_PRIVATE)
+        val edit = sharedPreferences?.edit()
+        val searchData = binding.searchView.setQuery(binding.searchView.query, false).toString()
+
+        // 데이터 저장
+        edit?.putString("searchData", searchData)
+        edit?.apply()
+    }
+
+
+    fun sharedLoad(){
+        val sharedPreferences = activity?.getSharedPreferences("searchText", Context.MODE_PRIVATE)
+        // default값 설정해줘야함 -> 데이터가 null일떄(존재하지 않을떄의 값)
+        // getString을 이용해 데이터를 꺼내서 사용
+        val searchData = sharedPreferences?.getString("searchData", null)
+
+        binding.searchView.setQuery(searchData, false)
+    }
 }
+
 
